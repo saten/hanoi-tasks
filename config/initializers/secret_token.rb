baea9c82e637c5ad67da067512cdf4b09491397b
saten <saten.r@gmail.com>
2010-11-01 11:17:25 +0100

    initial skel of hanoi tasks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HanoiTasks::Application.config.secret_token = 'dfa6bb2116ea9b8dbd7e486ee2b70d4695a26fde5842a956976e2f429ee8b0c8c64fe534e7c4866db26c9efaa8522821344352aa6f3c7490f9e28dfefbb72504'
