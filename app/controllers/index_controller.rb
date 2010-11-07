1a386a87f6470fd425ae4136bac9ced068eb72a3
saten <saten.r@gmail.com>
2010-11-07 15:35:38 +0100

    added jquery-rails

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -9,8 +9,6 @@ class IndexController < ApplicationController
       total_points=total_points+t.points
       completed_points=completed_points+t.points if t.completed
     end
-    logger.debug completed_points
-    logger.debug total_points
     render :text=>(completed_points.to_f/total_points.to_f)*100, :status=>:ok
   end
 end
